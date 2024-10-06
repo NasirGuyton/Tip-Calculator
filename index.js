a4b80941c252e422d10db3c85e64d6fb60aa181b
NasirGuyton <nasirjoseph.guyton@students.dominican.edu>
2024-10-06 10:16:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const bill = document.querySelector('#bill');
+const tip = document.querySelector('#tip');
+const numPeople = document.querySelector('#num-people');
+const tipTotal = document.querySelector('#tip-total');
+const total = document.querySelector('#total');
+
+const leftColumn = document.querySelector('.column-left');
+
+const inputs = document.querySelectorAll("input");
+const buttons = document.querySelectorAll('button');
+const nudgeInputs = document.querySelectorAll('.nudge-input');
+
+
+function handleBill(e) {
+  let tipAmount = Number(bill.value * (tip.value / 100));
+  let totalPrice = Number(tipAmount) + Number(bill.value);
+  tipTotal.textContent = "$" + (tipAmount / numPeople.value).toFixed(2);
+  total.textContent = "$" + (totalPrice / numPeople.value).toFixed(2);
+};
+
+function nudgeValue(inputName, action) {
+  let val = parseInt(document.querySelector(`#${inputName}`).value);
+  val = action == "increment" ? val + 1 : val - 1;
+  document.querySelector(`#${inputName}`).value = val;
+}
+
+inputs.forEach(input => {
+  input.addEventListener('input', handleBill)
+})
+
+nudgeInputs.forEach(element => {
+  element.onclick = function(e) {
+    if (e.target && e.target.classList.contains('decrement')) {
+      nudgeValue(e.target.dataset.name, 'decrement')
+    } else if (e.target && e.target.classList.contains('increment')) {
+      nudgeValue(e.target.dataset.name, 'increment')
+    }
+    handleBill();
+  }
+})
